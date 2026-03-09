fc6472ab5bc6b23897f33f7a3208406a11db2f8b
salmenahoe <salmenahoemma@gmail.com>
2026-03-09 12:01:28 +0200

    Add files via upload

---
+++ batman.py
@@ -0,0 +1,16 @@
+print("Moi kaiki")
+nimi = input("Anna nimesi (laita ensimäinen kirjain isolla)\n")
+if nimi == "Elina":
+    print("Moi bämäntä!!!")
+elif nimi == "Emma":
+    print("Moi toinen bämäntä!!")
+elif nimi == "Amanda":
+    print("Moi biker girl!")
+elif nimi == "Kaisa":
+    print("Moi tuleva kapteeni!")
+elif nimi == "Wäinö":
+    print("Moi UwU girl!")
+elif nimi == "Heikki":
+    print("Moi Hutsu!!")
+else:
+    print("Anna oikea nimi!!!!!")
